dc7c528c7687af008874054161c16fcce0d5100a
Imran Mansur <imranmansur217@gmail.com>
2026-05-19 18:20:16 +0600

    data type program added

---
+++ data-type.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;
+        double cgpa = 3.20;
+        char grade = 'A';
+        string name = "Imran";
+        bool isStudent = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("CGPA: " + cgpa);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Is Student: " + isStudent);
+    }
+}
